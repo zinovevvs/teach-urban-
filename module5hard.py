fe8cff87228e9821e2ea3711cef85e48482bf01a
zinovevvs <ziryanov97@gmail.com>
2024-07-09 11:42:43 +0500

    version5re

--- module5hard.py
+++ module5hard.py
@@ -1,48 +1,3 @@
-# class User:
-#     """
-#     Объект класса User
-#     """
-#
-#     def __init__(self, nickname, password, age):
-#         self.nickname = str(nickname)
-#         self.password = hash(password)
-#         if age < 18:
-#             return f"Вам нет 18 лет, пожалуйста покиньте страницу"
-#         else:
-#             self.age = int(age)
-#
-# class Video:
-#     def __init__(self, title, duration, adult_mode=False):
-#         self.title = title
-#         self.duration = duration
-#         self.time_now = 0
-#         self.adult_mode = adult_mode
-
-# ur = UrTube()
-# v1 = Video('Лучший язык программирования 2024 года', 200)
-# v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
-#
-# # Добавление видео
-# ur.add(v1, v2)
-#
-# # Проверка поиска
-# print(ur.get_videos('лучший'))
-# print(ur.get_videos('ПРОГ'))
-#
-# # Проверка на вход пользователя и возрастное ограничение
-# ur.watch_video('Для чего девушкам парень программист?')
-# ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-# ur.watch_video('Для чего девушкам парень программист?')
-# ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-# ur.watch_video('Для чего девушкам парень программист?')
-#
-# # Проверка входа в другой аккаунт
-# ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-# print(ur.current_user)
-#
-# # Попытка воспроизведения несуществующего видео
-# ur.watch_video('Лучший язык программирования 2024 года!')
-
 import hashlib
 import time
 
@@ -52,12 +7,19 @@ class User:
         self.password = hashlib.sha256(password.encode()).hexdigest()
         self.age = age
 
+    def __str__(self):
+        return self.nickname
+
+
 class Video:
     def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
-        self.time_now = 0
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return self.title
+
 
 class UrTube:
     def __init__(self):
