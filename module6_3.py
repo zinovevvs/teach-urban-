d2eed28702f08701a658219518e89d712b2891b6
zinovevvs <ziryanov97@gmail.com>
2024-07-27 15:14:35 +0500

    Update module6_3.py

--- module6_3.py
+++ module6_3.py
@@ -18,13 +18,16 @@ class Eagle:
 
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)
